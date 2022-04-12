20998362a4fe524df3f854a8feb3565f472742f8
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-04-12 09:37:46 +0200

    resolved:  problem with loginImage showing after login

--- src/pages/direct/auth/index.tsx
+++ src/pages/direct/auth/index.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
+import { useSelector } from 'react-redux';
 import styles from './index.module.css';
 import HomePage from '../home';
 import LoginImage from '../../../components/loginImage';
+import { RootState } from '../../../store';
 
 function AuthPage() {
+  const user = useSelector((state: RootState) => state.auth.user);
+  const isLoggedIn = user?.completed;
   return (
 
     <Grid container spacing={3} className="App" sx={{ display: 'flex', flexDirection: 'row', height: '100%' }}>
-      <Grid className={styles.left} item xs={12} md={6}>
-        <LoginImage />
-      </Grid>
-      <Grid xs={12} md={6}>
-        <HomePage />
-      </Grid>
+      {!isLoggedIn ? (
+        <>
+          <Grid className={styles.left} item xs={12} md={6}>
+            <LoginImage />
+          </Grid>
+          <Grid xs={12} md={6}>
+            <HomePage />
+          </Grid>
+        </>
+      ) : (
+        <Grid xs={12} md={12}>
+          <HomePage />
+        </Grid>
+      ) }
     </Grid>
   );
 }
