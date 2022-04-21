d093610f34ff464c32650c7359ce4dc6b1f1e185
lina <lina7906@gmail.com>
2022-04-21 18:44:59 +0200

    update: redirect home page

--- src/pages/redirect/Request.tsx
+++ src/pages/redirect/Request.tsx
@@ -12,6 +12,7 @@ export function HandleRequest() {
   const url = new URL(currentURL);
   const currentPath = url.pathname + url.search;
   const user = useSelector((state: RootState) => state.auth.user);
+  const isLoggedIn = user?.completed;
   const dispatch = useDispatch();
   const handleClick = () => dispatch(doLogin(currentPath));
 
@@ -26,17 +27,28 @@ export function HandleRequest() {
   }
   return (
     <div className={styles.main}>
-      <div className={styles.headline}>
+      <div className={styles.header}>
         <div className={styles.logo}>
           <OakLogo />
         </div>
       </div>
       <div className={styles.body}>
+        <div className={isLoggedIn ? styles.notshowtext1 : styles.showtext1}>
+          <div className={styles.word1}>
+            Share your data with
+            <div className={styles.word2}> BNP Paribas</div>
+          </div>
+        </div>
         <button type="button" className={styles.button} onClick={handleClick}>
           <div className={styles.buttontext}>
             Continue
           </div>
         </button>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.text5}>
+          Project Oak is a governmental initiative that allows you to store and transfer digital information between public and private organtisations.
+        </div>
       </div>
     </div>
   );
