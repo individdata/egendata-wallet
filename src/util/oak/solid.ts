35a9514f13ba4b16d4b7b82e7cde1d0064b11a12
Tamas Szabo <tamszagot@gmail.com>
2022-04-26 12:09:19 +0200

    feat: add more solid methods, datarequests

--- src/util/oak/solid.ts
+++ src/util/oak/solid.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 import {
   fetch,
 } from '@inrupt/solid-client-authn-browser';
@@ -7,31 +8,61 @@ function isOk(response: Response): boolean {
 }
 
 export async function postFile(url: RequestInfo, data: RequestInit, contentType = 'text/plain') {
-  //console.log("url: ", url);
-  let writeResponse = await fetch(url, {
-      method: "POST",
-      body: data.body,
-      headers: { "Content-type": contentType }
+  // console.log("url: ", url);
+  const writeResponse = await fetch(url, {
+    method: 'POST',
+    body: data.body,
+    headers: { 'Content-type': contentType },
   });
-  if(!isOk(writeResponse)) {
-      console.log(writeResponse.url, writeResponse.status, writeResponse.statusText);
-      throw new Error("Failed to POST: " + writeResponse.statusText);    
+  if (!isOk(writeResponse)) {
+    console.log(writeResponse.url, writeResponse.status, writeResponse.statusText);
+    throw new Error(`Failed to POST: ${writeResponse.statusText}`);
   } else {
-      console.log("POST: " + writeResponse.url);
+    console.log(`POST: ${writeResponse.url}`);
   }
   const body = await writeResponse.text();
-  return {data: body, contentType: writeResponse.headers.get("Content-Type")};
+  return { data: body, contentType: writeResponse.headers.get('Content-Type') };
+}
+
+export async function putFile(url: RequestInfo, data: RequestInit, contentType = 'text/plain') {
+  // console.log("url: ", url);
+  const writeResponse = await fetch(url, {
+    method: 'PUT',
+    body: data.body,
+    headers: { 'Content-type': contentType },
+  });
+  if (!isOk(writeResponse)) {
+    console.log(writeResponse.url, writeResponse.status, writeResponse.statusText);
+    throw new Error(`Failed to PUT: ${writeResponse.statusText}`);
+  } else {
+    console.log(`PUT: ${writeResponse.url}`);
+  }
+}
+
+export async function patchFile(url: RequestInfo, data: RequestInit, contentType = 'text/plain') {
+  // console.log("url: ", url);
+  const writeResponse = await fetch(url, {
+    method: 'PATCH',
+    body: data.body,
+    headers: { 'Content-type': contentType },
+  });
+  if (!isOk(writeResponse)) {
+    console.log(writeResponse.url, writeResponse.status, writeResponse.statusText);
+    throw new Error(`Failed to PATCH: ${writeResponse.statusText}`);
+  } else {
+    console.log(`PATCH: ${writeResponse.url}`);
+  }
 }
 
 export async function deleteFile(url: RequestInfo) {
-  //console.log("url: ", url);
-  let deleteResponse = await fetch(url, {
-      method: "DELETE"
+  // console.log("url: ", url);
+  const deleteResponse = await fetch(url, {
+    method: 'DELETE',
   });
-  if(!isOk(deleteResponse)) {
-      console.log(deleteResponse.url, deleteResponse.status, deleteResponse.statusText);
-      throw new Error("Failed to DELETE: " + deleteResponse.statusText);    
+  if (!isOk(deleteResponse)) {
+    console.log(deleteResponse.url, deleteResponse.status, deleteResponse.statusText);
+    throw new Error(`Failed to DELETE: ${deleteResponse.statusText}`);
   } else {
-      console.log("DELETE: " + deleteResponse.url);
+    console.log(`DELETE: ${deleteResponse.url}`);
   }
 }
