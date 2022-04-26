5170e63e2db396ac17fae62418f23f3ce5bfc362
Tamas Szabo <tamszagot@gmail.com>
2022-04-26 21:14:49 +0200

    feat: createOutboundDataRequest

--- src/util/oak/datarequest.ts
+++ src/util/oak/datarequest.ts
@@ -1,4 +1,5 @@
 import { egendataPrefixTurtle } from './egendata';
+import { putFile } from './solid';
 
 export type InboundDataRequest = {
   id: string,
@@ -8,44 +9,86 @@ export type InboundDataRequest = {
   purpose: string,
   returnUrl: string,
 };
-export const inboundDataRequestUrl = ((userPodUrl: string, id: string) => `${userPodUrl}oak/requests/request-${id}`);
-export const inboundDataRequestTurtle = ((
-  id: string,
-  requestorWebId: string,
-  providerWebId: string,
-  documentType: string,
-  purpose: string,
-  returnUrl: string,
-) => `
+const inboundDataRequestUrl = ((userPodUrl: string, id: string) => `${userPodUrl}oak/requests/request-${id}`);
+const inboundDataRequestTurtle = ((dataRequest: InboundDataRequest) => `
 ${egendataPrefixTurtle}
 <> a <egendata:InboundDataRequest> ;
-  <egendata:id> "${id}" ;
-  <egendata:requestorWebId> "${requestorWebId}" ;
-  <egendata:providerWebId> "${providerWebId}" ;
-  <egendata:documentType> "${documentType}" ;
-  <egendata:purpose> "${purpose}" ;
-  <egendata:returnUrl> "${returnUrl}" .`);
+  <egendata:id> "${dataRequest.id}" ;
+  <egendata:requestorWebId> "${dataRequest.requestorWebId}" ;
+  <egendata:providerWebId> "${dataRequest.providerWebId}" ;
+  <egendata:documentType> "${dataRequest.documentType}" ;
+  <egendata:purpose> "${dataRequest.purpose}" ;
+  <egendata:returnUrl> "${dataRequest.returnUrl}" .`);
 
-export const outboundDataRequestUrl = ((userPodUrl: string, id: string) => `${userPodUrl}/oak/fetch-requests/fetch-request-${id}`);
-export const outboundDataRequestTurtle = ((userPodUrl: string, responseUuid: string, pnr: string, documentTypeUrl: string) => `
+export async function storeInboundRequest(userPod: string, request: InboundDataRequest) {
+  const requestUrl = inboundDataRequestUrl(userPod, request.id);
+  const requestData = inboundDataRequestTurtle(request);
+  await putFile(
+    requestUrl,
+    { body: requestData },
+    'text/turtle',
+  );
+}
+
+export type OutboundDataRequest = {
+  id: string,
+  documentType: string,
+  dataSubjectIdentifier: string,
+};
+
+const outboundDataRequestUrl = ((userPodUrl: string, id: string) => `${userPodUrl}/oak/fetch-requests/fetch-request-${id}`);
+const dataLocationUrl = ((userPodUrl: string, id: string) => `${userPodUrl}/oak/responses/response-${id}.ttl`);
+const outboundDataRequestTurtle = ((id: string, documentType: string, dataSubjectIdentifier: string, dataLocation:string) => `
 ${egendataPrefixTurtle}
 <> a <egendata:OutboundDataRequest> ;
-  <egendata:documentType> ${documentTypeUrl} ;
-  <egendata:dataSubjectIdentifier> ${pnr} ;
-  <egendata:dataLocation> ${userPodUrl}/oak/responses/response-${responseUuid}.ttl .
-`);
+  <egendata:id> "${id}" ;
+  <egendata:documentType> "${documentType}" ;
+  <egendata:dataSubjectIdentifier> "${dataSubjectIdentifier}" ;
+  <egendata:dataLocation> "${dataLocation}" .`);
 
-export const outboundDataRequestLinkUrl = ((sourcePodUrl: string, id: string) => `${sourcePodUrl}/oak/inbox/request-${id}`);
-export const outboundDataRequestLinkTurtle = ((userPodUrl: string, id: string) => `
+export async function storeOutboundRequest(userPod: string, request: OutboundDataRequest) {
+  const requestUrl = outboundDataRequestUrl(userPod, request.id);
+  const locationUrl = dataLocationUrl(userPod, request.id);
+  const requestData = outboundDataRequestTurtle(request.id, request.documentType, request.dataSubjectIdentifier, locationUrl);
+  await putFile(
+    requestUrl,
+    { body: requestData },
+    'text/turtle',
+  );
+}
+
+const outboundDataRequestLinkUrl = ((sourcePodUrl: string, id: string) => `${sourcePodUrl}/oak/inbox/request-${id}`);
+const outboundDataRequestLinkTurtle = ((requestUrl: string) => `
 ${egendataPrefixTurtle}
-<> <egendata:OutboundDataRequest> <${userPodUrl}/oak/fetch-requests/fetch-request-${id}>.
+<> <egendata:OutboundDataRequest> <${requestUrl}>.
 `);
 
-export const inboundDataResponseUrl = ((userPodUrl: string, id: string) => `${userPodUrl}/oak/feetch-responses/fetch-response-${id}`);
-export const inboundDataResponseTurtle = (() => '');
+export async function storeOutboundRequestLink(id: string, userPod: string, sourcePod: string) {
+  const requestUrl = outboundDataRequestUrl(userPod, id);
+  const requestLinkUrl = outboundDataRequestLinkUrl(sourcePod, id);
+  const requestLinkData = outboundDataRequestLinkTurtle(requestUrl);
+  await putFile(
+    requestLinkUrl,
+    { body: requestLinkData },
+    'text/turtle',
+  );
+}
 
-export const inboundDataResponseAclUrl = ((userPodUrl: string, id: string) => `${userPodUrl}/oak/responses/response-${id}`);
-export const inboundDataResponseAclTurtle = ((userPodUrl: string, id: string, userWebId: string, sourceWebId: string) => `
+const inboundDataResponseUrl = ((userPodUrl: string, id: string) => `${userPodUrl}/oak/feetch-responses/fetch-response-${id}`);
+const inboundDataResponseTurtle = (() => '');
+
+export async function storeInboundDataResponse(id: string, userPod: string) {
+  const responseUrl = inboundDataResponseUrl(userPod, id);
+  const responseData = inboundDataResponseTurtle();
+  await putFile(
+    responseUrl,
+    { body: responseData },
+    'text/turtle',
+  );
+}
+
+const inboundDataResponseAclUrl = ((userPodUrl: string, id: string) => `${userPodUrl}/oak/responses/response-${id}`);
+const inboundDataResponseAclTurtle = ((userPodUrl: string, id: string, userWebId: string, sourceWebId: string) => `
 <#user> a <http://www.w3.org/ns/auth/acl#Authorization>;
     <http://www.w3.org/ns/auth/acl#accessTo> <${userPodUrl}/oak/requests/request-${id}>;
     <http://www.w3.org/ns/auth/acl#agent> <mailto:user@example.com>, <${sourceWebId}>;
@@ -55,3 +98,13 @@ export const inboundDataResponseAclTurtle = ((userPodUrl: string, id: string, us
     <http://www.w3.org/ns/auth/acl#agent> <mailto:sink@example.com>, <${userWebId}/sink/profile/card#me>;
     <http://www.w3.org/ns/auth/acl#mode> <http://www.w3.org/ns/auth/acl#Write>, <http://www.w3.org/ns/auth/acl#Read>, <http://www.w3.org/ns/auth/acl#Control>.
 `);
+
+export async function storeInboundDataResponseAcl(id: string, userPod: string, userWebId: string, sourceWebId: string) {
+  const responseAclUrl = inboundDataResponseAclUrl(userPod, id);
+  const responseAclData = inboundDataResponseAclTurtle(userPod, id, userWebId, sourceWebId);
+  await putFile(
+    responseAclUrl,
+    { body: responseAclData },
+    'text/turtle',
+  );
+}
