60fbc73a9d405606ff40c9ce6d2bd0b2ce08e8e6
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2022-05-02 21:54:06 +0200

    fix: remove unnecessary container

--- src/components/loginImage/loginImageText/index.tsx
+++ src/components/loginImage/loginImageText/index.tsx
@@ -5,21 +5,15 @@ import styles from './index.module.css';
 function LoginImageText() {
   return (
     <Grid container>
-      <Grid className={styles.header} item xs={12} sx={{ marginBottom: '29px' }}>
+      <Grid className={styles.header} item xs={12}>
         <Grid sx={{ maxWidth: '340px' }}>
           <p className={styles.first}>Your data in your control</p>
         </Grid>
       </Grid>
-      <Grid
-        className={styles.container}
-        container
-        spacing={3}
-      >
-        <Grid item xs={12} className={styles.textContainer}>
-          <p>
-            Project Oak is a governmental initiative that allows you to store and transfer digital information between public and private organizations.
-          </p>
-        </Grid>
+      <Grid item xs={12} className={styles.textContainer}>
+        <p>
+          Project Oak is a governmental initiative that allows you to store and transfer digital information between public and private organizations.
+        </p>
       </Grid>
     </Grid>
   );
