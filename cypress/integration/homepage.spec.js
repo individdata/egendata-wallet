11fd6209668f68031da5a00fb078e1cc98698bb9
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-05-06 16:25:15 +0200

    fix: exploring env vars

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,16 +1,19 @@
-    describe("renders the landing page", () => {
+const HOME = Cypress.env("home")
+
+describe("renders the landing page", () => {
+
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
+  it("should render the landingPage", () => {
+    cy.get("#landingPage");
+    cy.get("div").and("have.class", HOME); });
+
+  it("test if loginbutton is clickable", () => {
+    cy.get("button").and("have.class", "Button_button__t7agX");
+    // button.click()
+  });
 
 
-    it("should render the landingPage", () => {
-        cy.visit("/")
-        cy.get("#landingPage")
-        cy.get("div").and("have.class", "HomePage_main__Io9UL")
-    })
-
-    it("test if loginbutton is clickable", () => {
-        cy.visit("/")
-          cy.get("button").and("have.class", "Button_button__t7agX")
-        // button.click()
-    })
-
-})
+});
