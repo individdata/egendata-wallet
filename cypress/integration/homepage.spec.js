76578c985e319c2a50bf094095f6b2bd84a365c3
jakobniinja <jakob.yaro@utb.ecutbildning.se>
2022-05-09 16:40:58 +0200

    fix: small clean-up

--- cypress/integration/homepage.spec.js
+++ cypress/integration/homepage.spec.js
@@ -1,14 +1,14 @@
 // const  LOGINBUTTON = Cypress.env("liBtn");
 // const  HOME = Cypress.env("home")
- const  SSN = Cypress.env("ssn")
-
-
-
+const SSN = Cypress.env("ssn");
 
 describe("renders the landing page", () => {
 
+  beforeEach(() => {
+    cy.visit("/");
+  });
+
   it("should render share your data with bnp page", () => {
-    cy.visit("/")
     cy.get("div").and("have.class", "LandingPage_word1__k2PgZ");
   });
 
@@ -18,40 +18,27 @@ describe("renders the landing page", () => {
       .click({ multiple: true, force: true });
   });
 
-
   it("solid css login flow", () => {
-    cy.visit("/")
-
     cy.get("button")
       .and("have.class", "Button_button__t7agX")
       .click({ multiple: true, force: true });
 
-    cy.get("h1").contains("Community Solid Server")
+    cy.get("h1").contains("Community Solid Server");
 
-    cy.get('#pno').clear();
+    cy.get("#pno").clear();
 
-    
     // change to your ssn in cypress.json
-    cy.get('#pno').type(SSN);
+    cy.get("#pno").type(SSN);
 
-
-
-    cy.get("button")
-      .click({ multiple: true, force: true });
-
+    cy.get("button").click({ multiple: true, force: true });
 
     /* 
       HAVE YOUR TEST-BANKID 
       READY AND DO A MANUAL LOGIN
     */
 
-    cy.wait(7000)
-    
-    cy.get("button")
-      .click({ multiple: true, force: true });
+    cy.wait(7000);
 
-
-
+    cy.get("button").click({ multiple: true, force: true });
   });
-
 });
