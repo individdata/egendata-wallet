03faf3810f36f79d27e994e841e42ac50b75bb89
Kim Eriksson <kim.eriksson@arbetsformedlingen.se>
2022-05-17 11:28:42 +0000

    Update sv.ts

--- src/react-intl/locales/sv.ts
+++ src/react-intl/locales/sv.ts
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 export default {
-  landingpage_title: '(sv) Share your data with ',
+  landingpage_title: 'Dela din data med ',
   landingpage_line1: '(sv) To handle your request you need to identify yourself.',
   landingpage_line2: '(sv) If you are a first time user of Project OAK an account will be created for you when you log in.',
   landingpage_link: '(sv) Project OAK terms & conditions',
