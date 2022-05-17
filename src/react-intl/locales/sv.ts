6a7cf023c8b88c9a11e2b369708affd5e43ac795
Kim Eriksson <kim.eriksson@arbetsformedlingen.se>
2022-05-17 16:00:22 +0000

    Update sv.ts

--- src/react-intl/locales/sv.ts
+++ src/react-intl/locales/sv.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 export default {
   landingpage_title: 'Dela din data med ',
-  landingpage_line1: '(sv) To handle your request you need to identify yourself.',
+  landingpage_line1: 'Påbörja din dataöverföring genom att identifiera dig nedan.',
   landingpage_line2: '(sv) If you are a first time user of Project OAK an account will be created for you when you log in.',
   landingpage_link: '(sv) Project OAK terms & conditions',
   landingpage_footer: '(sv) Project Oak is a governmental initiative that allows you to store and transfer digital information between public and private organtisations.',
