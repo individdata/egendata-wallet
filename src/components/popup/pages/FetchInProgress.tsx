fc4563e374b085e6ab1a9050ab5c10b0112e6254
Anton Wiklund <anton.wiklund@arbetsformedlingen.se>
2022-06-29 13:52:39 +0200

    fix: remove unused imports

--- src/components/popup/pages/FetchInProgress.tsx
+++ src/components/popup/pages/FetchInProgress.tsx
@@ -1,16 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { v4 as uuid } from 'uuid';
 import { useIntl } from 'react-intl';
 import { setPopupData } from '../../../slices/popupSlice';
 import styles from './FetchInProgress.module.css';
-import PopupButtons, { PopupButton } from '../PopupButtons';
+import PopupButtons from '../PopupButtons';
 import PopupContent from '../PopupContent';
 import { RootState } from '../../../store';
 import ActivityIndicator from '../../ui/ActivityIndicator';
 import useTimeout from '../../../hooks/useTimeout';
-import { getProcessByRequestId } from '../../../util/oak/egendata';
 
 type Props = {
   requestId: string,
