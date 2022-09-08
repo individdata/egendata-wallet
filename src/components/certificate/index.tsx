0632fb4d9f9c254661c0fc389dab6110598f0f1a
Linus Törngren <linus@etnolit.se>
2022-09-08 10:27:26 +0200

    fix: Line breaks in the fetch details preview stage.

--- src/components/certificate/index.tsx
+++ src/components/certificate/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Grid, Stack, Typography } from '@mui/material';
 import styles from './index.module.css';
 
 type Props = {
@@ -10,21 +10,21 @@ function Certificate(props: Props) {
   const { certificate } = props;
 
   const renderedLines = Object.keys(certificate).map((key) => (
-    <div key={key}>
-      <Grid container>
-        <Grid item xs={12} className={styles.line}>
-          {key}
-          &nbsp;
-          <div className={styles.lineValue}>{certificate[key]}</div>
-        </Grid>
-      </Grid>
-    </div>
+    <Typography variant="h6" component="p">
+      {key}
+      &nbsp;
+      <div className={styles.lineValue}>{certificate[key]}</div>
+    </Typography>
   ));
 
   return (
     <Grid container className={styles.stickyScroll}>
-      <div className={styles.title}>Unemployment certificate</div>
-      {renderedLines}
+      <Typography variant="h4" component="h2" sx={{ color: '#65D36E' }}>
+        Unemployment certificate
+      </Typography>
+      <Stack direction="column">
+        {renderedLines}
+      </Stack>
     </Grid>
   );
 }
